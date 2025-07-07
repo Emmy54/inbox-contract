635fe775a15e124d24531c7d607e275f2ef58a4c
unknown <ogunrindeayomide18@gmail.com>
2025-07-07 15:31:05 +0100

    initial commit

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { Web3 } = require('web3');
+const { interface, bytecode } = require('./compile');
+
+//updated web3 and hdwallet-provider imports added for convenience
+const provider = new HDWalletProvider(
+  'fury nurse wash zebra rather tape roast soup middle carbon color bless', // Replace with your mnemonic
+  'https://sepolia.infura.io/v3/ff75cb6ec8144ccbbec138f2c5ee8bc6' // Replace with your Infura project ID
+);
+// deploy code will go here
+const web3 = new Web3(provider); 
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+   provider.engine.stop(); //Stop the provider engine after deployment
+};
+deploy();
